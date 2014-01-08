84e7e560602a5a5daa6320d1abc4003d877c2d5c
Rafael Konlechner <r.konlechner@gmail.com>
2014-01-08 14:06:25 +0100

    #Bugfix: Adapt preconditions for Cleanup

--- src/test/java/util/Cleanup.java
+++ src/test/java/util/Cleanup.java
@@ -2,9 +2,8 @@ package util;
 
 /**
  * Cleans up, after LoadSimulator, in case you want to get rid of additional
- * user-credentials and files. Make sure, you have following files already in
- * the files directory (they will not be affected): files/client/upload.txt
- * files/fs#/short.txt files/fs#/long.txt files/fs#/multiline.txt
+ * user-credentials and unifies all files in the 'files' directory for easier
+ * manual testing across computers.
  */
 public class Cleanup {
 
@@ -12,9 +11,9 @@ public class Cleanup {
 
 		cleanup();
 	}
-	
+
 	public static void cleanup() {
-		
+
 		Util.resetUsers();
 		FileGenerator.resetDirectories();
 	}
